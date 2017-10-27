ecb4c5bb901c2550eae7f8aefb645eb089c7dc14
leon-good-life <leon.good.life@gmail.com>
2017-10-27 19:49:46 +0300

    Add redux-thunk #8

--- web-frontend/src/store/reducers/dishes.js
+++ web-frontend/src/store/reducers/dishes.js
@@ -3,6 +3,39 @@ const BEGIN_FETCH_DISHES = 'BEGIN_FETCH_DISHES';
 const FETCH_DISHES_SUCCESS = 'FETCH_DISHES_SUCCESS';
 const FETCH_DISHES_ERROR = 'FETCH_DISHES_ERROR';
 
+const FETCH_DISHES = 'FETCH_DISHES';
+
+
+// action creators
+const beginFetchDishes = () => ({ action: BEGIN_FETCH_DISHES });
+const fetchDishesSuccess = (dishes) => ({ action: FETCH_CONTACT_SUCCESS, dishes });
+const fetchDishesError = (error) => ({ action: FETCH_DISHES_ERROR, error });
+
+
+// ajax
+const getDishes = (token) => {
+  const url = window.location.origin + '/rest/dish';
+  let headers = {};
+  headers['X-Auth-Token'] = token;
+  return fetch(url, {headers})
+    .then(response => {
+      if(response.status >= 200 && response.status < 300){
+        return Promise.resolve(response.json());
+      }
+      return Promise.reject(new Error(response.statusText));
+    });
+};
+
+
+// thunk action creator
+const fetchDishes = (token) => ((dispatch) => {
+  return getDishes(token).then(
+    dishes => dispatch(fetchDishesSuccess(dishes)),
+    error => dispatch(fetchDishesError(error))
+  );
+});
+
+
 // reducer
 const dishes = (state = {dishes: [], isFetching: false}, action) => {
   switch(action.type){
--- web-frontend/src/store/store.js
+++ web-frontend/src/store/store.js
@@ -1,4 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers/root';
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+);
