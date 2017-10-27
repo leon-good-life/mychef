ec5b9f9c113ab1ed7c812252053b960c68cb1fd8
leon-good-life <leon.good.life@gmail.com>
2017-10-27 20:43:53 +0300

    Fix users reducer #8

--- web-frontend/src/store/reducers/users.js
+++ web-frontend/src/store/reducers/users.js
@@ -1,18 +1,20 @@
+import { GET_USER_REQUEST, GET_USER_SUCCSESS, GET_USER_ERROR } from '../actions/users';
+
 export const users = (state = {contact: null, isFetching: false}, action) => {
   switch(action.type){
-    case BEGIN_FETCH_CONTACT:
+    case GET_USER_REQUEST:
       return {
         isFetching: true,
         contact: [...state.contact]
       };
       break;
-    case FETCH_CONTACT_SUCCESS:
+    case GET_USER_SUCCSESS:
       return {
         isFetching: false,
         contact: action.contact
       };
       break;
-    case FETCH_CONTACT_ERROR:
+    case GET_USER_ERROR:
       return Object.assign({}, state);
       break;
     default:
