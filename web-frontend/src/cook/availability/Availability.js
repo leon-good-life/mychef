d09384b97a599d902ede710c1010673d18b243dc
leon-good-life <leon.good.life@gmail.com>
2017-10-30 20:40:29 +0200

    Implement "Available to order" #6

--- web-frontend/src/cook/availability/Availability.js
+++ web-frontend/src/cook/availability/Availability.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { updateAvailability, getDish } from '../ajax';
+import { updateAvailability, getDish } from '../../ajax';
 import RaisedButton from 'material-ui/RaisedButton';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
-import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import NumericInput from 'react-numeric-input';
+import './Availability.css';
 
 class Availability extends React.Component {
   constructor(props){
@@ -23,7 +24,7 @@ class Availability extends React.Component {
         loading: 'Loading...',
         availability: 'Dish availability',
         start: 'Start recieve orders',
-        quantity: 'How many you can deliver?',
+        quantity: 'How many can you deliver?',
         time: 'How long it takes to cook and deliver?',
         threeHours: '3 hours',
         halfDay: 'Half day',
@@ -48,33 +49,42 @@ class Availability extends React.Component {
     }
     const dishesPath = `/${this.props.lang}/cook/dishes/`;
     return (
-      <form style={{flexGrow:1, maxWidth:500, background: 'white',padding: '20px',borderRadius: '5px'}}
-            onSubmit={this.handleSubmit}>
-        <h1>{values.availability}</h1>
-        <h2>{values.quantity}</h2>
-
-        <h2>{values.time}</h2>
-        <SelectField value={this.props.lang} onChange={this.handleChange}>
-          <MenuItem 
-            value="THREE_HOURS" 
-            primaryText={values.threeHours} />
-          <MenuItem
-            value="HALF_DAY" 
-            primaryText={values.halfDay} />
-          <MenuItem
-            value="ONE_DAY" 
-            primaryText={values.oneDay} />
-        </SelectField>
-        <br/>
-        <RaisedButton label={values.start} 
-                      type="submit" 
-                      labelStyle={{textTransform: 'none'}}
-                      style={{margin: '0 5px'}} />
-        <RaisedButton label={values.cancel} 
-                      style={{margin: '0 5px'}}
-                      labelStyle={{textTransform: 'none'}}
-                      containerElement={<Link to={dishesPath} />} />
-      </form>
+      <MuiThemeProvider>
+        <form style={{flexGrow:1, maxWidth:500, background: 'white',padding: '20px',borderRadius: '5px'}}
+              onSubmit={this.handleSubmit}>
+          <h1>{values.availability}</h1>
+          <h2>{values.quantity}</h2>
+          <NumericInput value="1" className="form-control" style={{
+	input: {
+    boxSizing: 'border-box',
+    fontSize: '30px',
+    padding: '10px',
+    width: '100px'
+	}
+}} />
+          <h2>{values.time}</h2>
+          <SelectField value="THREE_HOURS">
+            <MenuItem 
+              value="THREE_HOURS" 
+              primaryText={values.threeHours} />
+            <MenuItem
+              value="HALF_DAY" 
+              primaryText={values.halfDay} />
+            <MenuItem
+              value="ONE_DAY" 
+              primaryText={values.oneDay} />
+          </SelectField>
+          <br/>
+          <RaisedButton label={values.start} 
+                        type="submit" 
+                        labelStyle={{textTransform: 'none'}}
+                        style={{margin: '0 5px'}} />
+          <RaisedButton label={values.cancel} 
+                        style={{margin: '0 5px'}}
+                        labelStyle={{textTransform: 'none'}}
+                        containerElement={<Link to={dishesPath} />} />
+        </form>
+      </MuiThemeProvider>
     );
   }
   componentDidMount(){
@@ -100,13 +110,6 @@ class Availability extends React.Component {
       this.props.history.push(dishesPath);
     });
   }
-  getChildContext() {
-    return {muiTheme: getMuiTheme(baseTheme)};
-  }
 }
-
-Availability.childContextTypes = {
-  muiTheme: PropTypes.object.isRequired
-};
 
 export default withRouter(Availability);
--- web-frontend/src/cook/dishes/Dish.js
+++ web-frontend/src/cook/dishes/Dish.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import FlatButton from 'material-ui/FlatButton';
 import {Card, CardActions, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import Toggle from 'material-ui/Toggle';
 import { Link } from 'react-router-dom';
 
 const Dish = ({lang, dish}) => {
@@ -24,7 +25,11 @@ const Dish = ({lang, dish}) => {
       <CardMedia overlay={<CardTitle title={dish.name} subtitle={dish.description} />}>
         <img src={dish.image} alt={dish.name} />
       </CardMedia>
-      <CardText>{values.price} {dish.price}</CardText>
+      <CardText>
+        {values.price} {dish.price}
+        <Toggle label="Avaiable to order" style={{display: 'inline-block', width: 'auto', marginLeft: 30}} labelPosition="right" />
+      </CardText>
+      
       <CardActions>
         <FlatButton label={values.edit} containerElement={<Link to={editDishPath} />} labelStyle={{textTransform: 'none'}} />
         <FlatButton label={values.delete} containerElement={<Link to={deleteDishPath} />} labelStyle={{textTransform: 'none'}} />
