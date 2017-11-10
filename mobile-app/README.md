66503563c671ac1b27af9fe0a0cafbc69f8bb699
leon-good-life <leon.good.life@gmail.com>
2017-11-10 13:04:07 +0200

    Remove all old code

--- mobile-app/README.md
+++
@@ -1,3 +0,0 @@
-Native mobile apps for iOS and Android devices.
-
-Under development.
--- web-frontend/src/admin/Loading.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Loading = () => (
-  <div className="admin-container">
-    <h1>Loading...</h1>
-  </div>
-);
-
-export default Loading;
